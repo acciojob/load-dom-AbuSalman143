2f6e88674697e454ada3bc697dd58103737acdf3
Abu Salman <121804012+AbuSalman143@users.noreply.github.com>
2023-12-30 13:25:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded",()=>{
+let msg=document.createElement("p");
+	msg.innerText="DOM load success";
+	document.body.appendChild(msg);
+}
+
+
+ 
